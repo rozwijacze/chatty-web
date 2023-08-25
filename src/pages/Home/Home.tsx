c898ed1f7e1d07f0f9623102251b293f790fcada
Adrian Kuznar <pospolight@gmail.com>
2023-08-25 16:55:48 +0200

    quick styling fix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,8 @@
-import { TopNav } from '../../components/TopNav';
 import { SideNav } from '../../components/SideNav';
 import { ChatWindow } from '../../components/ChatWindow';
-import { ThemeButton } from '../../components/ThemeButton';
-import { Footer } from '../../components/Footer';
 import './Home.scss';
 import { useTheme } from '../../hooks/useTheme';
+import { TopNav } from '../../components/TopNav/TopNav';
 
 export const Home = () => {
     const theme = useTheme();
@@ -14,8 +12,6 @@ export const Home = () => {
             <TopNav />
             <SideNav />
             <ChatWindow />
-            <Footer />
-            <ThemeButton toggleMode={theme.toggle} />
         </div>
     );
 };
