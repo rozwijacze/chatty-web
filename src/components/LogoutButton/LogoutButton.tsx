03c93c1026a6d82103f059a3d99c030930c658bc
Danilowicz <44084790+dereniowskid885@users.noreply.github.com>
2023-12-11 15:34:30 +0100

    Logout button styling (#43)
    Co-authored-by: Daniel Dereniowski <leinad885@o2.pl>

--- src/components/LogoutButton/LogoutButton.tsx
+++ src/components/LogoutButton/LogoutButton.tsx
@@ -1,15 +1,18 @@
 import { useAuth } from '../../contexts/AuthContext';
 import { isUserLoggedIn } from '../../utils/utils';
+import { ReactComponent as LogoutIcon } from '/src/assets/logout.svg';
+import './LogoutButton.scss';
 
-export default function ThemeButton() {
+export default function LogoutButton() {
     const { logout } = useAuth();
+
     function handleLogout() {
         isUserLoggedIn() ? logout() : null;
     }
 
     return (
-        <button className="button" onClick={handleLogout}>
-            Logout
+        <button className="logout-btn" onClick={handleLogout} title="Logout">
+            <LogoutIcon />
         </button>
     );
 }
