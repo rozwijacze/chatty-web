8178546c41c2fc77bf5dbf196010f29a9110deb2
Adrian Kuznar <pospolight@gmail.com>
2023-12-13 10:28:39 +0100

    useOnOutsideClick hook

---
+++ src/hooks/useOnOutsideClick.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+export default function useOnClickOutside(ref: React.RefObject<HTMLElement>, handler: (event: MouseEvent | TouchEvent) => void) {
+    useEffect(() => {
+        const listener = (event: MouseEvent | TouchEvent) => {
+            if (!ref.current || (ref.current as Node).contains(event.target as Node)) {
+                return;
+            }
+            handler(event);
+        };
+        document.addEventListener('mousedown', listener);
+        document.addEventListener('touchstart', listener);
+        return () => {
+            document.removeEventListener('mousedown', listener);
+            document.removeEventListener('touchstart', listener);
+        };
+    }, [ref, handler]);
+}
