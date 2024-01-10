f708dee05b00ea05b6344a184cbc4209a43e9188
Daniel Dereniowski <leinad885@o2.pl>
2024-01-10 14:49:48 +0100

    ThemeButton and LocaleButton changed to use of ButtonItem

--- src/components/layout/LocaleButton/LocaleButton.tsx
+++ src/components/layout/LocaleButton/LocaleButton.tsx
@@ -1,15 +1,14 @@
-import './LocaleButton.scss';
-import { Locale } from '@customTypes/Locale';
-import { LocaleContext, useLocaleContext } from '@contexts/LocaleContext';
+import Locale from '@customTypes/Locale';
+import { useLocaleContext } from '@contexts/LocaleContext';
 
 export default function LocaleButton() {
-    const { locale, toggle }: LocaleContext = useLocaleContext();
+    const { locale } = useLocaleContext();
 
     return (
-        <button className="locale-btn" onClick={toggle}>
-            <span className={`locale-btn__item ${locale === Locale.ENGLISH ? 'locale-btn__item--active' : ''}`}>{Locale.ENGLISH}</span>
+        <>
+            <span className={`locale ${locale === Locale.ENGLISH ? 'locale--active' : ''}`}>{Locale.ENGLISH}</span>
             <span>/</span>
-            <span className={`locale-btn__item ${locale === Locale.POLISH ? 'locale-btn__item--active' : ''}`}>{Locale.POLISH}</span>
-        </button>
+            <span className={`locale ${locale === Locale.POLISH ? 'locale--active' : ''}`}>{Locale.POLISH}</span>
+        </>
     );
 }
--- src/components/layout/ThemeButton/ThemeButton.tsx
+++
@@ -1,8 +0,0 @@
-import useTheme, { ThemeHook } from '@hooks/useTheme';
-import './ThemeButton.scss';
-
-export default function ThemeButton() {
-    const { toggle }: ThemeHook = useTheme();
-
-    return <button className="theme-btn" title="Toggle theme" onClick={() => toggle()}></button>;
-}
