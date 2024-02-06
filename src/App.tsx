e1d52433c3c53da87b1d71e0769061b3b226002d
Daniel Dereniowski <leinad885@o2.pl>
2024-02-06 18:58:30 +0100

    Lazy loading added to routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import Error from '@pages/Error/Error';
-import Home from '@pages/Home/Home';
-import FormLayout from '@layouts/FormLayout/FormLayout';
+import { lazy, Suspense, useEffect } from 'react';
 import ViewType from '@customTypes/ViewType';
 import { useAuthContext } from '@contexts/AuthContext';
+import LoadingLayout from '@layouts/LoadingLayout/LoadingLayout';
+import { setThemeOnBody } from '@helpers/ThemeHelper';
+
+const Error = lazy(() => import('@pages/Error/Error'));
+const Home = lazy(() => import('@pages/Home/Home'));
+const FormLayout = lazy(() => import('@layouts/FormLayout/FormLayout'));
 
 export default function App() {
+    useEffect(() => setThemeOnBody(), []);
     const { isAuthenticated } = useAuthContext();
+
+    const ROUTES = [
+        {
+            path: '*',
+            element: <Error />
+        },
+        {
+            path: '/',
+            element: isAuthenticated ? <Home /> : <FormLayout view={ViewType.LOGIN} />
+        },
+        {
+            path: '/register',
+            element: <FormLayout view={ViewType.REGISTER} />
+        }
+    ];
 
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="*" element={<Error />} />
-                <Route path="/" element={isAuthenticated ? <Home /> : <FormLayout view={ViewType.LOGIN} />} />
-                <Route path="/register" element={<FormLayout view={ViewType.REGISTER} />} />
-            </Routes>
+            <Suspense fallback={<LoadingLayout />}>
+                <Routes>
+                    {ROUTES.map((route, key) => (
+                        <Route key={key} path={route.path} element={route.element} />
+                    ))}
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     );
 }
