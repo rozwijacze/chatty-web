58b7d8ac10838939b61a20ec751ed16aec5a4335
Daniel Dereniowski <leinad885@o2.pl>
2024-02-20 17:09:44 +0100

    LocaleContext converted to use of Redux

---
+++ src/redux/slices/localeSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+import Locale from '@customTypes/Locale';
+
+const storageLocale = localStorage.getItem('locale');
+const defaultLocale = storageLocale || Locale.ENGLISH;
+
+// Saving default locale to localStorage on app init
+(() => {
+    if (!storageLocale) {
+        localStorage.setItem('locale', defaultLocale);
+    }
+})();
+
+const localeSlice = createSlice({
+    name: 'locale',
+    initialState: {
+        value: defaultLocale
+    },
+    reducers: {
+        toggleLocale(state) {
+            switch (state.value) {
+                default:
+                    state.value = Locale.ENGLISH;
+                    break;
+                case Locale.ENGLISH:
+                    state.value = Locale.POLISH;
+                    break;
+            }
+
+            localStorage.setItem('locale', state.value);
+        }
+    }
+});
+
+export const { toggleLocale } = localeSlice.actions;
+export default localeSlice.reducer;
