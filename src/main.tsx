3a5c2fbb6d12d38b3dc43b9c469f796069cfe6a9
Daniel Dereniowski <leinad885@o2.pl>
2024-02-20 17:44:56 +0100

    Redux and Redux Toolkit install, alias for redux folder added, redux store and redux hooks definition

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import '@styles/main.scss';
 import GlobalContextProviders from '@components/other/GlobalContextProviders/GlobalContextProviders';
+// import ReduxStoreProvider from '@components/other/ReduxStoreProvider/ReduxStoreProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
+        {/* <ReduxStoreProvider> */}
         <GlobalContextProviders>
             <App />
         </GlobalContextProviders>
+        {/* </ReduxStoreProvider> */}
     </React.StrictMode>
 );
